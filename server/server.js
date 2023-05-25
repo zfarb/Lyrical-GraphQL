0427c393faf860c06da896c943aba8a1363cab8c
zacharyfarber <zachfarber001@icloud.com>
2023-05-25 11:29:42 -0400

    connected mongo

--- server/server.js
+++ server/server.js
@@ -4,26 +4,32 @@ const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
+const keys = require('../keys');
 
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = keys.MONGO_URI;
 if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
+    throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+    .once('open', () => console.log('Connected to MongoDB Atlas instance.'))
+    .on('error', (error) =>
+        console.log('Error connecting to MongoDB Atlas:', error)
+    );
 
 app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
-}));
+app.use(
+    '/graphql',
+    expressGraphQL({
+        schema,
+        graphiql: true
+    })
+);
 
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
