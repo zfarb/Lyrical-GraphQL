d764109df29e9795091f3a6d6d47a836c9f2905e
zacharyfarber <zachfarber001@icloud.com>
2023-05-25 11:50:13 -0400

    hooked up apollo, created SongList component, updated README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lyrical-GraphQL
+
 Starter project from a GraphQL course on Udemy.com
+
+Project... Technologies are outdated. Can I refactor to use the updated versions?
--- client/index.js
+++ client/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import SongList from './components/SongList';
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+    return (
+        // ApolloProvider allows app to use Apollo
+        <ApolloProvider client={client}>
+            <div>
+                <SongList />
+            </div>
+        </ApolloProvider>
+    );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector('#root'));
