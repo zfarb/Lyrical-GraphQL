ea74ed517e4f65d228aa8994e5c165ce27aba0ea
zacharyfarber <zachfarber001@icloud.com>
2023-05-26 00:47:13 -0400

    passed lyrics to LyricList from SongDetail and mapping over lyrics in LyricList

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 
 class LyricList extends Component {
+    renderLyrics() {
+        const { lyrics } = this.props;
+
+        return lyrics.map(({ id, content }) => {
+            return (
+                <li key={id} className="collection-item">
+                    {content}
+                </li>
+            );
+        });
+    }
+
     render() {
-        return <ul>LyricList</ul>;
+        return <ul className="collection">{this.renderLyrics()}</ul>;
     }
 }
 
